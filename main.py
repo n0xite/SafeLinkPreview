6e7fe5ef00530c6f78613767eea62ece2fc92bdc
Antoni Łoniewski <n000xite@gmail.com>
2022-10-22 19:36:11 +0200

    Version 0.1.1

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 from selenium import webdriver
-from colorama import Fore, Back, Style
+from colorama import Fore
 from pyfiglet import Figlet
-from selenium.webdriver.chrome.service import Service
-import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from time import sleep
+import re
 
-
-link = ''
-webdriverPath = r'C:\Users\n000x\PycharmProjects\SLP'
+webdriverPath = r'C:\Users\n000x\PycharmProjects\SLP\SafeLinkPreview\geckodriver.exe'
+binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
 
 
 def Init():
-    slpSign = Figlet(font='isometric4')
+    slpSign = Figlet(font='isometric1')
 
     print(Fore.BLUE + slpSign.renderText('SLP'))
 
+    print('\n' + '\n' + '\n')
+    global link
+    link = input(Fore.BLUE + 'Welcome to Safe Link Preview! Please provide a link to preview: ' + Fore.CYAN)
+    return link
+
+
+
 class Driver:
-    service = Service(webdriverPath)
-    driver = webdriver.Chrome(service=service)
-    driverOptions = webdriver.ChromeOptions()
-    driverOptions.add_argument("--headless")
-    driverOptions.add_argument("--disable-gpu")
-    driverOptions.add_argument("--no-sandbox")
+    driverOptions = Options()
+    driverOptions.headless = True
+    driver = webdriver.Firefox(service=Service(webdriverPath), firefox_binary=binary, options=driverOptions)
 
+    def slp(self, l):
+        self.driver.get(l)
+        sleep(3)
+        self.driver.save_screenshot('SLP.png')
+        print(Fore.GREEN + 'Processing ...')
+        sleep(1)
+        print(Fore.GREEN + 'Finished !')
+        self.driver.close()
 
-    def slp(d):
-        d.get(link)
 
 if __name__ == "__main__":
     Init()
+    Driver.slp(Driver(),link)
